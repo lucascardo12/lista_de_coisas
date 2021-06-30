9bda49cd09ff380bcb27e2fcb57ac67db417006a
lucas cardoso <lucas.cardo123@hotmail.com>
2021-06-30 18:15:44 -0400

    Migrate listasController para Getx

--- lib/controller/listas-controller.dart
+++ lib/controller/listas-controller.dart
@@ -1,3 +1,4 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
@@ -6,9 +7,27 @@ import 'package:listadecoisa/model/user.dart';
 import 'package:listadecoisa/services/banco.dart';
 import 'package:listadecoisa/services/global.dart';
 
-class ListasController {
+class ListasController extends GetxController {
   final gb = Get.find<Global>();
   final banco = Get.find<BancoFire>();
+  final bool isComp = Get.arguments[1];
+  late AdmobBannerSize bannerSize;
+  final formKey = GlobalKey<FormState>();
+  final Coisas coisas = Get.arguments[0];
+  late FocusScopeNode node;
+  final FocusNode nodeText1 = FocusNode();
+  late AdmobInterstitial interstitialAd;
+  @override
+  void onInit() {
+    interstitialAd = AdmobInterstitial(
+      adUnitId: 'ca-app-pub-1205611887737485/8086429884',
+      listener: (AdmobAdEvent event, Map<String, dynamic>? args) {
+        if (event == AdmobAdEvent.closed) interstitialAd.load();
+      },
+    );
+    interstitialAd.load();
+    super.onInit();
+  }
 
   Future<void> criaCoisa({required Coisas coisa}) async {
     var auxi = gb.lisComp.indexWhere((element) => element.idLista == coisa.idFire);
@@ -20,9 +39,9 @@ class ListasController {
       await banco.criaAlteraCoisas(coisas: coisa, user: gb.usuario!);
     }
     Fluttertoast.showToast(
-        msg: coisa != null ? "Alterado com Sucesso!!" : "Criado com Sucesso!!",
+        msg: coisa.idFire != null ? "Alterado com Sucesso!!" : "Criado com Sucesso!!",
         toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.CENTER,
+        gravity: ToastGravity.TOP,
         timeInSecForIosWeb: 5,
         backgroundColor: gb.getPrimary(),
         textColor: Colors.white,
