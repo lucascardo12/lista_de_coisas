408616e0ce2dcc93f61343865271494a83d0de47
lucas <lucas.cardo123@hotmail.com>
2021-08-31 13:38:13 -0400

    inclusão do serviço de notificação local

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:listadecoisa/services/admob.dart';
+import 'package:listadecoisa/services/alarme.dart';
 import 'package:listadecoisa/services/banco.dart';
 import 'package:listadecoisa/services/global.dart';
+import 'package:listadecoisa/services/notificacao.dart';
 import 'package:listadecoisa/view/cadastroPage.dart';
 import 'package:listadecoisa/view/compartilha-page.dart';
 import 'package:listadecoisa/view/homePage.dart';
@@ -11,6 +13,8 @@ import 'package:listadecoisa/view/loginPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Get.putAsync(() => AlarmeLista().inicia());
+  await Get.putAsync(() => NotificacaoLista().inicia());
   await Get.putAsync(() => AdMob().inicia());
   await Get.putAsync(() => Global().inicia());
   await Get.putAsync(() => BancoFire().inicia());
