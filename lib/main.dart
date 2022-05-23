0b49e08b66931bf9978733113c66c8acfbe9b589
lucas cardoso <lucas.cardo123@hotmail.com>
2022-05-23 17:55:16 -0400

    register modules and refatoring routers

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:listadecoisa/services/admob.dart';
-import 'package:listadecoisa/services/banco.dart';
+import 'package:get_it/get_it.dart';
+import 'package:listadecoisa/modules/auth/auth_module.dart';
+import 'package:listadecoisa/modules/auth/presenter/ui/pages/cadastro_page.dart';
+import 'package:listadecoisa/modules/auth/presenter/ui/pages/login_page.dart';
+import 'package:listadecoisa/modules/home/home_module.dart';
+import 'package:listadecoisa/modules/listas/listas_module.dart';
+import 'package:listadecoisa/modules/splash/splash_module.dart';
+import 'package:listadecoisa/modules/splash/ui/splash_page.dart';
 import 'package:listadecoisa/services/global.dart';
-import 'package:listadecoisa/view/cadastro_page.dart';
-import 'package:listadecoisa/view/compartilha_page.dart';
-import 'package:listadecoisa/view/home_page.dart';
-import 'package:listadecoisa/view/listas_page.dart';
-import 'package:listadecoisa/view/login_page.dart';
+import 'package:listadecoisa/modules/home/presenter/ui/pages/compartilha_page.dart';
+import 'package:listadecoisa/modules/home/presenter/ui/pages/home_page.dart';
+import 'package:listadecoisa/modules/listas/presenter/ui/pages/listas_page.dart';
+import 'package:listadecoisa/services/service_module.dart';
+
+GetIt di = GetIt.instance;
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Get.putAsync(() => AdMob().inicia());
-  await Get.putAsync(() => Global().inicia());
-  await Get.putAsync(() => BancoFire().inicia());
+  ServiceModule().register();
+  AuthModule().register();
+  HomeModule().register();
+  ListasModule().register();
+  SplashModule().register();
   runApp(MyApp());
 }
 
-class MyApp extends GetView {
-  final gb = Get.find<Global>();
+class MyApp extends StatelessWidget {
+  final gb = di.get<Global>();
 
   MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
       title: 'Lista de Coisas',
@@ -40,32 +47,16 @@ class MyApp extends GetView {
             )
             .copyWith(secondary: gb.getSecondary()),
       ),
-      getPages: [
-        GetPage(
-          name: '/',
-          page: () => gb.usuario != null ? HomePage() : Login(),
-        ),
-        GetPage(
-          name: '/comp',
-          page: () => CompartilhaPage(),
-        ),
-        GetPage(
-          name: '/cadastro',
-          page: () => Cadastro(),
-        ),
-        GetPage(
-          name: '/listas',
-          page: () => ListasPage(),
-        ),
-        GetPage(
-          name: '/home',
-          page: () => HomePage(),
-        ),
-        GetPage(
-          name: '/login',
-          page: () => Login(),
-        )
-      ],
+      initialRoute: SplashPage.route,
+      routes: {
+        '/': (context) => const SplashPage(),
+        SplashPage.route: (context) => const SplashPage(),
+        CompartilhaPage.route: (context) => const CompartilhaPage(),
+        CadastroPage.route: (context) => const CadastroPage(),
+        ListasPage.route: (context) => const ListasPage(),
+        HomePage.route: (context) => const HomePage(),
+        LoginPage.route: (context) => const LoginPage(),
+      },
     );
   }
 }
